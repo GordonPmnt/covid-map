7d3d8669c329d64f7c8f2ea4cd0c8e99819b5d80
Gordon Preumont <preumontgr@gmail.com>
2020-04-03 15:32:47 +0200

    display points on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,22 +1,42 @@
 import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Source, Layer } from 'react-map-gl';
 import settings from '../settings'
+
 
 const Map = () => {
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
-    latitude: 50.8549541,
-    longitude: 4.3053504,
+    latitude: 51,
+    longitude: 5,
     zoom: 3
   });
 
+  const data = require('../data/datatest.json')
+
   return (
-    <ReactMapGL
-      {...viewport}
-      onViewportChange={setViewport}
-      mapboxApiAccessToken={settings.mapBoxToken}
-    />
+    <>
+      <ReactMapGL
+        {...viewport}
+        onViewportChange={setViewport}
+        mapboxApiAccessToken={settings.mapBoxToken}
+      >
+        <Source
+          id="covidData"
+          type="geojson"
+          data={data}
+        >
+          <Layer
+            id="point"
+            type="circle"
+            paint={{
+              'circle-radius': 5,
+              'circle-color': 'red'
+              }} 
+          />
+        </Source>
+      </ReactMapGL>
+    </>
   );
 }
 
